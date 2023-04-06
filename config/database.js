57cc22152a4eee384ca7147cb9a45513e4644a5a
Judy Hanson <judy.l.hanson@gmail.com>
2023-04-06 12:54:52 -0700

    added date to the user model schema

--- config/database.js
+++ config/database.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
+      useUnifiedTopology: true      
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
+// const mainRoutes = require('./routes/main')
+// const todoRoutes = require('./routes/todos')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -24,13 +24,16 @@ app.use(express.json())
 app.use(logger('dev'))
 // Sessions
 app.use(
-    session({
-      secret: 'keyboard cat',
+  session({
+      secret: "keyboard cat",
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
+      store: MongoStore.create({
+          mongoUrl: process.env.DB_STRING
+      }),
+  })
+);
+
   
 // Passport middleware
 app.use(passport.initialize())
@@ -38,8 +41,8 @@ app.use(passport.session())
 
 app.use(flash())
   
-app.use('/', mainRoutes)
-app.use('/todos', todoRoutes)
+// app.use('/', mainRoutes)
+// app.use('/todos', todoRoutes)
  
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
